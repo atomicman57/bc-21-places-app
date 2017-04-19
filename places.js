4b468842c7308605708e141b783dff75d82b7ed4
Mayowa Makinde <Mayowamakinde53@gmail.com>
2017-04-19 06:53:53 +0100

    initial interface commitff

---
+++ places.js
@@ -0,0 +1,23 @@
+// Function to get user's current longitude and Latitude
+window.onload = function() {
+  var startPos;
+  var geoOptions = {
+     timeout: 10 * 1000
+  }
+
+  var geoSuccess = function(position) {
+    startPos = position;
+    console.log(startPos.coords.latitude);
+    console.log(startPos.coords.longitude);
+  };
+  var geoError = function(error) {
+    console.log('Error occurred. Error code: ' + error.code);
+    // error.code can be:
+    //   0: unknown error
+    //   1: permission denied
+    //   2: position unavailable (error response from location provider)
+    //   3: timed out
+  };
+
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+};
