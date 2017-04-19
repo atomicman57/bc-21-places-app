79bd842a6023dec8ac33a19b42092db934c630a8
Mayowa Makinde <Mayowamakinde53@gmail.com>
2017-04-19 14:06:54 +0100

    adding function to Seach by input

--- places.js
+++ places.js
@@ -1,4 +1,10 @@
+// ********************************************************************
+// ********************************************************************
+// ********************************************************************
+// ********************************************************************
 // Function to get user's current longitude and Latitude
+var lat
+var lon
 window.onload = function() {
   var startPos;
   var geoOptions = {
@@ -7,8 +13,8 @@ window.onload = function() {
 
   var geoSuccess = function(position) {
     startPos = position;
-    console.log(startPos.coords.latitude);
-    console.log(startPos.coords.longitude);
+    lat = startPos.coords.latitude;
+    lon = startPos.coords.longitude;
   };
   var geoError = function(error) {
     console.log('Error occurred. Error code: ' + error.code);
@@ -21,3 +27,49 @@ window.onload = function() {
 
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
 };
+// ********************************************************************
+// ********************************************************************
+// ********************************************************************
+// ********************************************************************
+// Function to Search based on places entered by the user
+var map;
+var service;
+var infowindow;
+
+function initialize() {
+var entry = document.getElementById('entry').value
+// alert(entry)
+  var pyrmont = new google.maps.LatLng(lat,lon);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    query: entry
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+  console.log(status);
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      console.log(place);
+    }
+  }
+}
+//   else {
+//     alert("Mayowa...Do something about error message!")
+//   }
+// }
+// ********************************************************************
+// ********************************************************************
+// ********************************************************************
+// ********************************************************************
+// Function to Sort Search by establishment
